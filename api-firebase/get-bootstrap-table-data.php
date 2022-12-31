c0f9b52c863b558d3034bf525822aeeb46b15214
divaharsha <divakarvan03@gmail.com>
2022-12-31 12:09:07 +0530

    initial

---
+++ api-firebase/get-bootstrap-table-data.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+// set time for session timeout
+$currentTime = time() + 25200;
+$expired = 3600;
+
+// if session not set go to login page
+if (!isset($_SESSION['username'])) {
+    header("location:index.php");
+}
+
+// if current time is more than session timeout back to login page
+if ($currentTime > $_SESSION['timeout']) {
+    session_destroy();
+    header("location:index.php");
+}
+
+// destroy previous session timeout and create new one
+unset($_SESSION['timeout']);
+$_SESSION['timeout'] = $currentTime + $expired;
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
+include_once('../includes/custom-functions.php');
+$fn = new custom_functions;
+include_once('../includes/crud.php');
+include_once('../includes/variables.php');
+$db = new Database();
+$db->connect();
+
+if (isset($config['system_timezone']) && isset($config['system_timezone_gmt'])) {
+    date_default_timezone_set($config['system_timezone']);
+    $db->sql("SET `time_zone` = '" . $config['system_timezone_gmt'] . "'");
+} else {
+    date_default_timezone_set('Asia/Kolkata');
+    $db->sql("SET `time_zone` = '+05:30'");
+}
+
+
+$db->disconnect();
