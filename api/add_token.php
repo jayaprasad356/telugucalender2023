dd85896341a2f8db4a6c1ded7cb59ec244195fa9
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-24 11:04:43 +0530

    add_token.php

--- api/add_token.php
+++ api/add_token.php
@@ -6,26 +6,31 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
 date_default_timezone_set('Asia/Kolkata');
 
 include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 
-if (empty($_POST['fcm_id'])) {
-    $response['success'] = false;
-    $response['message'] = "FCM ID is Empty";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (empty($_POST['fcm_id'])) {
+        $response['success'] = false;
+        $response['message'] = "FCM ID is Empty";
+        print_r(json_encode($response));
+        return;
+    }
+
+    $fcm_id = $db->escapeString($_POST['fcm_id']);
+
+    $sql = "INSERT INTO `device_token` (fcm_id) VALUES ('$fcm_id')";
+    $db->sql($sql);
+
+    $response['success'] = true;
+    $response['message'] = "Device token added Successfully";
     print_r(json_encode($response));
-    return false;
+} else {
+    $response['success'] = false;
+    $response['message'] = "Invalid request method";
+    print_r(json_encode($response));
 }
-
-$fcm_id = $db->escapeString($_POST['fcm_id']);
-
-$sql = "INSERT INTO `device_token` (fcm_id) VALUES ('$fcm_id')";
-$db->sql($sql);
-
-$response['success'] = true;
-$response['message'] = "Device token added Successfully";
-print_r(json_encode($response));
 ?>
