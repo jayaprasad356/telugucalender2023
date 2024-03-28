cd06190d68e6939d0628c6aae293a1e781f18151
Akraja712 <ashokkumar7hitter@gmail.com>
2024-03-28 11:12:44 +0530

    grahanalu_list.php

---
+++ api/rudhraksha_list.php
@@ -0,0 +1,47 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+
+include_once('../includes/crud.php');
+$db = new Database();
+$db->connect();
+
+
+
+$sql = "SELECT * FROM `rudhraksha_tab`";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if($num>=1){
+        $rows = array();
+        $temp = array();
+        foreach ($res as $row) {
+            $id = $row['id'];
+            $temp['id'] = $row['id'];
+            $temp['title'] = $row['title'];
+            $temp['description'] = $row['description'];
+            $sql = "SELECT * FROM `rudhraksha_tab_variant` WHERE rudhraksha_tab_id = '$id'";
+            $db->sql($sql);
+            $res = $db->getResult();
+            $temp['rudhraksha_tab_variant'] = $res;
+            $rows[] = $temp;
+        }
+        $response['success'] = true;
+        $response['message'] = "Rudhraksha Listed Successfully";
+        $response['data'] = $rows;
+        print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Data Not Found";
+    print_r(json_encode($response));
+}
+
+
+?>
